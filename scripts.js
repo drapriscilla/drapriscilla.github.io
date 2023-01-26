3dad256acf6a26fd02830a533f75e5638183e49b
drapriscilla <123639784+drapriscilla@users.noreply.github.com>
2023-01-26 12:19:02 -0300

    first upload

---
+++ scripts.js
@@ -0,0 +1,22 @@
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top - 70
+    }, 820);
+});
+
+window.addEventListener('scroll', scrollFunction);
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.querySelector(".navlogoimg").style.height = "40px";
+    document.querySelector(".navbar").style.height = "70px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.querySelector(".navlogoimg").style.height = "70px";
+    document.querySelector(".navbar").style.height = "100px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
